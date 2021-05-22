b8c983e02b0bcdd6000516a82f24b0bfc5838517
Horea <horea.constandache@3pillarglobal.com>
2021-05-22 15:12:13 +0300

    minor review

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,14 +2,11 @@ import { ThemeProvider } from "@material-ui/core";
 import React, { FunctionComponent } from "react";
 
 import theme from "../utils/theme";
-import RecipeCard from "./components/recipie-card/RecipieCard";
 
 const Home: FunctionComponent<Object> = () => {
   return (
    <ThemeProvider theme={theme}>
-     <div style={{marginTop: '20px'}}>
-        <RecipeCard title="Title text"/>
-     </div>
+    
     </ThemeProvider>
   )
 }

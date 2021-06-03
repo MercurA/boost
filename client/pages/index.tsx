61a6787baec10bc270aa806f4e7955ae5d1f6542
MercurA <constandacheh@gmail.com>
2021-06-04 00:22:59 +0300

    minor changes

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { ThemeProvider } from "@material-ui/core";
 import { FunctionComponent } from "react";
-
-import theme from "../utils/theme";
 
 const Home: FunctionComponent<unknown> = () => {
   return (
-   <ThemeProvider theme={theme}>
-      {''}
-    </ThemeProvider>
+    <></>
   )
 }
 

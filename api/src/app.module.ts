c09d9467e425f0a1a48e2109816fe3e34bce846d
MercurA <constandacheh@gmail.com>
2021-06-12 19:47:37 +0300

    added migration configs

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,15 @@
+import "reflect-metadata";
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import DatabaseConfig from './configdb';
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(DatabaseConfig)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
